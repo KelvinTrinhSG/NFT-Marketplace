ebe87aab514f1b6dffa37b6dafd87e2f8445c25f
KelvinTrinh <thkien85@Gmail.com>
2024-04-29 16:15:27 +0700

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+import { Navbar } from "../components/Navbar";
+import { Planq } from "@thirdweb-dev/chains";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "Polygon Amoy Testnet";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider activeChain={Planq}>
+      <ChakraProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
